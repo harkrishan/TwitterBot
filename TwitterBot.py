b1e5040e07879e6c3f4e62998f06bcafd616bae5
Harkrishan Singh <harkrishan23@gmail.com>
2015-08-12 23:52:50 +0530

    Version 1.2

--- TwitterBot.py
+++ TwitterBot.py
@@ -32,7 +32,13 @@ file = "/twitterBot/max_id.txt"
 def wolfram_alpha(str):
         str=str.lower()
         str=str.replace("@berry_bots"," ")
+        str=str.replace("."," ")
+        str=str.replace("hi berry bots"," ")
+        str=str.replace("hello berry bots"," ")
+        str=str.replace("berry bots"," ")
+        str=str.replace("berry bot"," ")
         str=str.strip()
+        str=str.encode('utf-8')
         res = client.query(str)
         if len(res.pods)>0:
                 text=" "
@@ -49,23 +55,41 @@ def wolfram_alpha(str):
 max=open(file,"r")
 max.seek(0,0)
 mid=max.read(18)
+
+search_results = None
+
 try:
         search_results = twitter.search(q="@berry_bots",lang="en",count=5,since_id=str(mid))
-except TwythonError as e:
-        print e
+
+except TwythonError as err:
+        print err
+
 max.close()
+
 mxd = None
 
 try:
-        for tweet in reversed(search_results["statuses"]):
-                mxd = tweet["id_str"]
-                answer = wolfram_alpha(tweet["text"])
-                if answer!='x':
-                        reply_id = tweet["id_str"]
-                        reply_user = '@' + tweet['user']['screen_name']
-                        answer = reply_user + ' ' + answer
-                        twitter.update_status(status=answer,in_reply_to_status_id=reply_id)
-                        time.sleep(5)
+        if search_results:
+                for tweet in reversed(search_results["statuses"]):
+                        mxd = tweet["id_str"]
+                        answer = wolfram_alpha(tweet["text"])
+                        if answer!='x':
+                                reply_id = tweet["id_str"]
+                                reply_user = '@' + tweet['user']['screen_name']
+                                reply_user = reply_user.encode('utf-8')
+                                if len(answer)>123:
+                                        n=123
+                                        for i in range(0, len(answer), n):
+                                                ans=answer[i:i+n]
+                                                ans = reply_user + ' ' + ans
+                                                twitter.update_status(status=ans,in_reply_to_status_id=reply_id)
+                                                time.sleep(5)
+                                else:
+                                        answer = reply_user + ' ' + answer
+                                        twitter.update_status(status=answer,in_reply_to_status_id=reply_id)
+                                        time.sleep(5)
+
+
 except TwythonError as e:
         print e
 
@@ -73,3 +97,4 @@ if mxd:
         max=open(file,"w")
         max.write(mxd)
         max.close()
+
